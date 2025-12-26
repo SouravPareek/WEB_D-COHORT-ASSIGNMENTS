ee07bcf4bc762a57d5b7d0fb4c19bb1a60f72307
SouravPareek <souravpareek1510@gmail.com>
2025-12-26 20:25:31 +0530

    day 68 task

---
+++ 068 React task/src/components/HeroTemp.jsx
@@ -0,0 +1,27 @@
+const Hero = () => {
+  return (
+    <main className='Hero'>
+        <div className="top">
+            <h1>Unleash Your Inner Champion Today.</h1>
+            <h1>All In One Place.</h1>
+            <p>Join the ultimate tennis experience - where passion meets performance.</p>
+            <p>and every swing brings you closer to victory</p>
+            <button>Start your own journey</button>
+        </div>
+        <div className="btm">
+            <div className="btm-left">
+                <p>Train with real professionals.</p>
+                <p>Get the real results.</p>
+            </div>
+            <div className="btm-right">
+                <a href="#">Instagram<i class="ri-arrow-right-up-line"></i></a>
+                <a href="#">Facebook<i class="ri-arrow-right-up-line"></i></a>
+                <a href="#">Tik Tok<i class="ri-arrow-right-up-line"></i></a>
+            </div>
+        </div>
+
+    </main>
+  )
+}
+
+export default Hero
---
+++ 068 React task/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+const Nav = () => {
+  return (
+    <nav>
+        <h1>Horizon Courts</h1>
+        <div className='nav-links'>
+            <a href="#">About Us</a>
+            <a href="#">Services</a>
+            <a href="#">Coaches</a>
+            <a href="#">Events</a>
+            <a href="#">Contacts</a>
+        </div>
+        <button>Book now <i class="ri-arrow-right-up-line"></i></button>
+    </nav>
+  )
+}
+
+export default Nav
